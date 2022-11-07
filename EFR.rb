4289254bc2710357e992cc300752ef0a0bd268d8
SetScript <101998415+SetScript@users.noreply.github.com>
2022-11-07 15:56:42 +0300

    example of a Ruby programming language, EFR
    this code is written in the Ruby programming language I created a fork with which you can make small programs

---
+++ EFR.rb
@@ -0,0 +1,15 @@
+
+#the variables on which everything rests I don't know how but okay
+ab="0"
+aw="0"
+#methods, best friends of variables
+def move
+    gets
+end
+def bring ab
+    puts ab
+end
+def wait aw
+    sleep aw
+end
+#the input line here cannot be walked higher :3
